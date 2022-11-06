2741a14203cd181a2519e3511bff004580e03bc2
runtime <72543405+RAFhub6@users.noreply.github.com>
2022-11-06 00:04:14 +0000

    more commands

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 var display = document.getElementById("display")
 var vm = new VM(document.getElementById("vm"))
+
+if (localStorage.getItem("disk")) vm.device.disk = JSON.parse(JSON.stringify(JSON.parse(localStorage.getItem("disk"))))
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault();
+    localStorage.setItem("disk", JSON.stringify(vm.device.disk))
+});
--- vm/vm.js
+++ vm/vm.js
@@ -4,7 +4,13 @@ class VM {
     this.device.disk = {
       "1": {
         name: "prim-part",
-        contents: {}
+        contents: {
+          "bcd": {
+            'type': 'file',
+            'filename': 'bcd',
+            'path': '1/bcd'
+          }
+        }
       }
     }
     this.device.exts = {}
@@ -18,14 +24,68 @@ class VM {
     }
     this.functions = {}
     this.functions.fs= {}
+    this.functions.gimp = {}
+    //
     this.functions.fs.readPart = (args) => {
       return this.device.disk[args[0]]
     }
+    //
     this.functions.fs.listPart = () => {
       return this.device.disk
     }
+    //
     this.functions.fs.readFile = (args) => {
-      return this.device.disk[args[0].split("/")[0]].contents[]
+     if (this.device.disk[args[0].split("/")[0]].contents[args[0]]){
+        return this.device.disk[args[0].split("/")[0]].contents[args[0]]
+     } else {
+      return "File does not exist" 
+     }
     }
+    //
+    this.functions.fs.writeFile = (args) => {
+      if (this.device.disk[args[0].split("/")[0]].contents[args[0]]){
+        this.device.disk[args[0].split("/")[0]].contents[args[0]].data += "\r\n " + args[1]
+      } else {
+        if (args[0].split("/").length -  1 > 1){
+          if (this.device.disk[args[0].split("/")[0]].contents[args[0].split("/").length - 2]){
+            if (this.device.disk[args[0].split("/")[0]].contents[args[0].split("/").length - 2].type == 'folder'){
+              this.device.disk[args[0].split("/")[0]].contents[args[0]] = {
+               'type': 'file',
+               'filename': args[0].split('/')[args[0].split('/').length - 1],
+               'path': args[0],
+               'data': args[1],
+               'created': Date("now")
+              }
+            } else {
+              return "File is not a folder"
+            }
+          } else {
+            return "Folder does not exist"
+          }
+        } else {
+          this.device.disk[args[0].split("/")[0]].contents[args[0]] = {
+             'type': 'file',
+             'filename': args[0].split('/')[args[0].split('/').length - 1],
+             'path': args[0],
+             'data': args[1],
+             'created': Date("now")
+          }
+        }
+      }
+    }
+    //
+    this.functions.fs.makeDir = (args) => {
+      if (!this.device.disk[args[0].split("/")[0]].contents[args[0]]){
+        this.device.disk[args[0].split("/")[0]].contents[args[0]] = {
+             'type': 'folder',
+             'filename': args[0].split('/')[args[0].split('/').length - 1],
+             'path': args[0],
+             'created': Date("now")
+        } 
+      } else {
+         return "Already exists"
+      }
+    }
+    //
   }
 }
